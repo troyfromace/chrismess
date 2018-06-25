fe8fc06dad5d2537ef1773b06509e2311d62fb6a
Troy <troy@troywiegand.com>
2018-06-25 13:30:58 -0400

    Completed Day 1 HW Vanilla [Button changes Header]

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const button = document.querySelector('button')
+
+const changeHeader = function() {
+    const h1 = document.querySelector('h1')
+    h1.textContent='It\'s raining Chris!'
+}
+
+button.addEventListener('click', changeHeader)
