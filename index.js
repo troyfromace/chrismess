e26d6beaefdb1806de420cc43f60b7400d2beae7
Troy <troy@troywiegand.com>
2018-06-25 13:51:27 -0400

    Super Mega Bonus [Made Form That Updates Header Whn a Button is Clicked]

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const button = document.querySelector('button')
+const button = document.querySelector('#firstButton')
+const Favebutton = document.querySelector('#faveButton')
+const h1 = document.querySelector('#MEN')
 
 const changeHeader = function() {
-    const h1 = document.querySelector('#MEN')
+   
     h1.textContent='It\'s raining Chris!'
 }
 
+Favebutton.addEventListener('click',Faveomatic)
 button.addEventListener('click', changeHeader)
+
+function Faveomatic() {
+    var x = document.getElementById("faveForm");
+    var faveOutput = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+        faveOutput += x.elements[i].value;
+    }
+    h1.textContent=faveOutput
+}
