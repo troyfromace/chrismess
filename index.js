02847a695bffe31047fabb16e46d6ad9291d67e3
Troy <troy@troywiegand.com>
2018-06-26 10:56:38 -0400

    div shennagians and inner HTML

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-//All my DOM Goodies
-const button = document.querySelector('#firstButton')
-const h1 = document.querySelector('#MEN')
-const faveText =document.querySelector('#faveForm')
+const form = document.querySelector('form#flickForm')
 
-//for the first two parts
-const changeHeader = function() {
-   
-    h1.textContent='It\'s raining Chris!'
-}
-button.addEventListener('click', changeHeader)
-
-//Super Mega Bonus Credit
-
-
-function Faveomatic() {
-    
-    event.preventDefault();
-    var x = document.getElementById("faveForm");
-    var faveOutput = "";
-    var i;
-    for (i = 0; i < x.length ;i++) {
-        faveOutput += x.elements[i].value;
-    }
-    h1.textContent=faveOutput
+const changeHeading = function(ev) {
+  ev.preventDefault()
+  const f = ev.target
+  const flicksDiv = document.querySelector('#flicks')
+  flicksDiv.innerHTML += '<p>' + f.flickName.value + '</p>'
 }
 
-//Super Mega Bonus Credit Hyper Fighting
-
-
-faveText.addEventListener('submit',Faveomatic)
+form.addEventListener('submit', changeHeading)
