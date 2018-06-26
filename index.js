b079c0eac4c6206989b39d08e6dbb1a9ae1dc573
Troy <troy@troywiegand.com>
2018-06-26 13:03:05 -0400

    Displays the Chris Count from the second form field

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form#flickForm')
-
-
+const Display = document.querySelector('#ChrisCountDisplay')
+var ChrisStorm = 0
 
 const listAdder9000 = function(ev) {
   ev.preventDefault()
@@ -12,7 +12,10 @@ const listAdder9000 = function(ev) {
 
   item.textContent=flickName
   list.appendChild(item)
-   
+  
+  ChrisStorm += Number(f.ChrisCount.value)
+  Display.textContent="CHRIS COUNT :  "+ChrisStorm
+  
    f.reset()
 }
 
