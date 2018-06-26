a60d455b6d00432dc51c7a37a483f4fde2c19113
Troy <troy@troywiegand.com>
2018-06-26 11:10:50 -0400

    Reset the form after each submission

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const form = document.querySelector('form#flickForm')
 
+
+
 const changeHeading = function(ev) {
   ev.preventDefault()
   const f = ev.target
+
+  const flickName=f.flickName.value
   const flicksDiv = document.querySelector('#flicks')
-  flicksDiv.innerHTML += '<p>' + f.flickName.value + '</p>'
+   flicksDiv.innerHTML += `<p>${flickName}</p>`
+   
+   f.reset()
 }
 
 form.addEventListener('submit', changeHeading)
