7bd5d6c72737414804da7f18fc15f199b227d34d
Troy <troy@troywiegand.com>
2018-06-26 13:52:02 -0400

    Clean formSubmiter9000 to have multiple helper functions

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ const form = document.querySelector('form#flickForm')
 const Display = document.querySelector('#ChrisCountDisplay')
 var ChrisStorm = 0
 
-const listAdder9000 = function(ev) {
-  ev.preventDefault()
-  const f = ev.target
-
+function listOtron57(f) {
   const flickName=f.flickName.value
   const list = document.querySelector('#flicks')
   let item=document.createElement('li')
-
   item.textContent=flickName
   list.appendChild(item)
-  
+}
+
+function chrisCountOMatic(f){
   ChrisStorm += Number(f.ChrisCount.value)
   Display.textContent="CHRIS COUNT :  "+ChrisStorm
-  
+}
+
+const formSubmiter9000 = function(ev) {
+  ev.preventDefault()
+  const f = ev.target
+
+  listOtron57(f);
+  chrisCountOMatic(f);
    f.reset()
 }
 
-form.addEventListener('submit', listAdder9000)
+form.addEventListener('submit', formSubmiter9000)
