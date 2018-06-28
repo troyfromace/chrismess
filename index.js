ca56d1b5ecc3aff23cd798ea03bce163cf63ae20
Troy <troy@troywiegand.com>
2018-06-28 12:56:38 -0400

    Refactor Code to get rid of some global variables

--- index.js
+++ index.js
@@ -1,86 +1,36 @@
-var ChrisStorm=0;
-var filmArray=[];
+
 var chopArray=[];
 var listCount=0;
 class App{
   constructor() {
-this.load()
+    this.list=document.querySelector('#flicks')
+
+    this.filmArray = []
+    this.load()
+
     const form = document.querySelector('form#flickForm')
     form.addEventListener('submit', (ev)=>{
       ev.preventDefault()
-      
-      this.handleSubmit(ev,ChrisStorm)
-
-      if(listCount===0){}
-      else {
-        const deleteButton=document.querySelector(`button.delete${listCount}`)
-        deleteButton.addEventListener('click', (ev)=>{
-          ev.preventDefault()
-         const d= ev.target
-        console.log(d.getAttribute('class'))
-        let deleteClass=''
-        if(d.getAttribute('class')===null)
-        { 
-          deleteClass=d.parentElement.parentElement.getAttribute('class')
-        }
-        else{
-        deleteClass=d.getAttribute('class')
-        }
-        deleteClass=deleteClass.slice(7)
-        deleteClass='.'+deleteClass
-        let chopNumber=deleteClass.slice(7)
-        chopNumber=Number(chopNumber)
-        let choppity=chopArray.lastIndexOf(chopNumber)
-        filmArray.splice(choppity,1)
-        chopArray.splice(choppity,1)
-        const deleteItem=document.querySelector(deleteClass)
-        const list=document.querySelector('#flicks')
-        list.removeChild(deleteItem)
-        this.chrisCountOMatic(ChrisStorm)
-        this.sendToStorage(filmArray)
-
+      this.handleSubmit(ev)
       })
-      }
-
-      if(listCount===0){}
-      else {
-        const list=document.querySelector('#flicks')
-        const faveButton=document.querySelector(`button.fave${listCount}`)
-        faveButton.addEventListener('click', (ev)=>{
-          ev.preventDefault()
-         const l= ev.target
-        console.log(l.getAttribute('class'))
-        let faveClass=''
-        if(l.getAttribute('class')===null)
-        { 
-          faveClass=l.parentElement.parentElement.getAttribute('class')
-        }
-        else{
-        faveClass=l.getAttribute('class')
-        }
-        faveClass=faveClass.slice(5)
-        faveClass='.'+faveClass
-        const faveItem=document.querySelector(faveClass)
-        faveItem.classList.toggle("SUPERLOVE")
-        faveButton.textContent='Maybe Not :('
-        let faveArraySpot=faveClass.slice(5)
-        faveArraySpot=Number(faveArraySpot)
-        let abc=chopArray.lastIndexOf(faveArraySpot)
-        filmArray[abc].fave= !filmArray[abc].fave
-        console.log(filmArray)
-        this.sendToStorage(filmArray)
-
-      })
-      }
-
-      })
- 
   }
 
+  save(){
+    localStorage.setItem('stringyArray', JSON.stringify(this.filmArray));
+    }
 
-chrisCountOMatic(ChrisStorm){
- 
-  filmArray.forEach(function(flick){
+    load(){
+      const flicks=JSON.parse(localStorage.getItem('stringyArray')) 
+      
+      if(flicks){
+        flicks.forEach(flick => this.addFlick(flick))
+        this.chrisCountOMatic()
+      }
+     }
+
+chrisCountOMatic(){
+  var ChrisStorm=0;
+  this.filmArray.forEach(function(flick){
 ChrisStorm+=Number(flick.count)
   })
   document.querySelector('#ChrisCountDisplay').textContent="CHRIS COUNT :  "+ChrisStorm
@@ -111,7 +61,7 @@ renderItem(flick) {
     item.appendChild(span)
   })
 /////////////////////
-  const deleteButton=this.deleteButtonGenerator(flick)
+  const deleteButton=this.deleteButtonGenerator(flick,item)
   item.appendChild(deleteButton)
   item.classList.add(`delete${listCount}`)
 
@@ -119,28 +69,74 @@ renderItem(flick) {
   const faveButton=this.faveButtonGenerator(flick)
   item.classList.add(`fave${listCount}`)
   item.appendChild(faveButton)
+  const list=document.querySelector('#flicks')
+       
+
+     
   return item
 }
 
 faveButtonGenerator(flick){
-  const faveButton=document.createElement('button')
-  faveButton.innerHTML='<i class="fas fa-grin-hearts"></i>'
-  faveButton.classList.add('fave')
-  faveButton.classList.add(`fave${listCount}`)
-  return faveButton
+  const Button=document.createElement('button')
+  Button.innerHTML='<i class="fas fa-grin-hearts"></i>'
+  Button.classList.add('fave')
+  Button.classList.add(`fave${listCount}`)
+  Button.addEventListener('click', (ev)=>{
+    ev.preventDefault()
+   const l= ev.target
+  console.log(l.getAttribute('class'))
+  let faveClass=''
+  if(l.getAttribute('class')===null)
+  { 
+    faveClass=l.parentElement.parentElement.getAttribute('class')
+  }
+  else{
+  faveClass=l.getAttribute('class')
+  }
+  faveClass=faveClass.slice(5)
+  faveClass='.'+faveClass
+  const faveItem=document.querySelector(faveClass)
+  faveItem.classList.toggle("SUPERLOVE")
+  Button.textContent='Maybe Not :('
+  let faveArraySpot=faveClass.slice(5)
+  faveArraySpot=Number(faveArraySpot)
+  let abc=chopArray.lastIndexOf(faveArraySpot)
+  this.filmArray[abc].fave= !(this.filmArray[abc].fave)
+  this.save(this.filmArray)
+})
+  return Button
 }
 
-deleteButtonGenerator(flick){
+deleteButtonGenerator(flick, item){
   const button=document.createElement('button')
   button.classList.add('delete')
   button.innerHTML='<i class="far fa-trash-alt"></i>'
   button.classList.add(`delete${++listCount}`)
   chopArray.push(listCount)
+  debugger
+        
+        button.addEventListener('click', (ev)=>{
+          
+          this.removeFlick(flick, item)
+
+      })
   return button
 }
 
+removeFlick(flick, item) {
+  // remove from the UI
+  this.list.removeChild(item)
 
-handleSubmit(ev,ChrisStorm) {
+  // remove from the array
+  debugger
+  const i = this.filmArray.indexOf(flick)
+  this.filmArray.splice(i, 1)
+  this.chrisCountOMatic()
+  // update localStorage
+  this.save()
+}
+
+handleSubmit(ev) {
   const f = ev.target
 
   const flick = {
@@ -153,30 +149,34 @@ handleSubmit(ev,ChrisStorm) {
   {
     document.querySelector('html').classList.add('fantasticFour')
   }
-  filmArray.push(flick);
-  console.log(filmArray);
+  this.addFlick(flick)
+
   const item = this.renderItem(flick)
 
-  const list = document.querySelector('#flicks')
-  list.appendChild(item)
-
-  this.chrisCountOMatic(ChrisStorm)
-  this.sendToStorage(filmArray)
+  this.chrisCountOMatic()
+  this.save(this.filmArray)
   f.reset()
+  f.flickName.focus()
 }
 
-sendToStorage(filmArray){
-const stringyArray= JSON.stringify(filmArray)
-console.log(stringyArray)
-localStorage.setItem('stringyArray', stringyArray);
 
+
+
+
+ addFlick(flick){
+this.filmArray.push(flick)
+const item = this.renderItem(flick)
+
+    // mark it as a favorite, if applicable
+    if (flick.fave) {
+      item.classList.add('SUPERLOVE')
+    }
+debugger
+    // add it to the DOM
+    this.list.appendChild(item)
+  }
 }
 
-load(){
- const flicks=JSON.parse(localStorage.getItem('stringyArray')) 
- filmArray=flicks || []
-}
-}
 
 
 const app = new App()
